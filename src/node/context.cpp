abebd26a72e8e6356da13ba44f5452517c958185
jdawg24 <78548840+jdawg24@users.noreply.github.com>
2024-03-15 14:44:23 +0000

    Update context.cpp

--- src/node/context.cpp
+++ src/node/context.cpp
@@ -11,6 +11,7 @@
 #include <net.h>
 #include <net_processing.h>
 #include <netgroup.h>
+#include <node/sv2_template_provider.h>
 #include <policy/fees.h>
 #include <scheduler.h>
 #include <txmempool.h>
