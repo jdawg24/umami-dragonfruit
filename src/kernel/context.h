0e3cb6dff3df45a158d2334a522ccec7ef0f17c3
jdawg24 <admin@moonpool.io>
2024-04-03 10:05:25 +0100

    /src/ipc-kernel

--- src/kernel/context.h
+++ src/kernel/context.h
@@ -5,20 +5,19 @@
 #ifndef BITCOIN_KERNEL_CONTEXT_H
 #define BITCOIN_KERNEL_CONTEXT_H
 
+#include <util/signalinterrupt.h>
+
 #include <memory>
 
 namespace kernel {
 //! Context struct holding the kernel library's logically global state, and
-//! passed to external libsugarchain_kernel functions which need access to this
+//! passed to external libbitcoin_kernel functions which need access to this
 //! state. The kernel library API is a work in progress, so state organization
 //! and member list will evolve over time.
 //!
 //! State stored directly in this struct should be simple. More complex state
 //! should be stored to std::unique_ptr members pointing to opaque types.
 struct Context {
-    //! Declare default constructor and destructor that are not inline, so code
-    //! instantiating the kernel::Context struct doesn't need to #include class
-    //! definitions for all the unique_ptr members.
     Context();
     ~Context();
 };
